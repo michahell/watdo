dcfff9bff9a28278fd5f7c95dd5f250afc1698e3
Michael <maggelo@gmail.com>
2016-06-05 11:23:22 +0200

    initial WIP commit.

---
+++ watdo.js
@@ -0,0 +1,11 @@
+var watdo = (function () { // eslint-disable-line
+  var module = {}; // eslint-disable-line
+
+  module.someVar = false;
+
+  module.someFunction = function () {
+    return true;
+  };
+
+  return module;
+})();
